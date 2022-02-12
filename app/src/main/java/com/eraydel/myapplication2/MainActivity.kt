d47c8b748e86c64ff7948849249ba6e973298d1c
Erick Ayala Delgadillo <erick.ayala.d@sat.gob.mx>
2022-02-11 20:26:43 -0600

    se agregan variables en log para debugeo

--- app/src/main/java/com/eraydel/myapplication2/MainActivity.kt
+++ app/src/main/java/com/eraydel/myapplication2/MainActivity.kt
@@ -2,10 +2,14 @@ package com.eraydel.myapplication2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val a = 5
+        Log.i("Information" , "El valor de a es $a")
+        Log.i("Information" , "El valor de a*3 es ${a*3} ")
     }
 }
